73e945713d78d17c2865d9068fdcc2f489db1f8f
Sun <Sun.Minvs@gmail.com>
2024-09-30 11:29:34 +0700

    30/09/24 Update
    - Added return_speed variable to draggable obj script
    - Added return_multiplier variable to draggable obj script

--- draggableObj.gd
+++ draggableObj.gd
@@ -1,31 +1,30 @@
 extends Sprite2D
 
 var dragging = false
-var of = Vector2(0, 0) ## Offset
+var of = Vector2(0, 0) 
 var original_position = Vector2(0, 0)
 var returning = false
-var return_speed = 200.0
 
-# Called when the node is ready
+@export var return_speed = 200.0
+@export var return_multiplier = 1.0
+
 func _ready() -> void:
 	original_position = global_position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if dragging:
 		position = get_global_mouse_position() - of
+		returning = false 
 	elif returning:
-		# Move the object back to its original position smoothly
-		global_position = global_position.move_toward(original_position, return_speed * delta)
+		global_position = global_position.move_toward(original_position, return_speed * return_multiplier * delta)
 		if global_position.distance_to(original_position) < 1.0:
 			returning = false
-			global_position = original_position  # Snap to original position
+			global_position = original_position 
 
 func _on_button_button_down() -> void:
 	dragging = true
-	returning = false  # Stop returning if dragging starts again
 	of = get_global_mouse_position() - global_position
 
 func _on_button_button_up() -> void:
 	dragging = false
-	returning = true  # Start returning to original position
+	returning = true
