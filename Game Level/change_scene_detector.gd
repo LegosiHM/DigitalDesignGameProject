56e1f59d623dbea5cb608410a74e07d469467a99
LegosiHM <105794916+LegosiHM@users.noreply.github.com>
2024-10-11 22:28:39 +0700

    Fixed Bug

--- Game Level/change_scene_detector.gd
+++ Game Level/change_scene_detector.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var entered = false
-@export var my_scene: PackedScene
+@export var my_scene: String
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
 	entered = true
@@ -11,4 +11,4 @@ func _on_body_exited(body: PhysicsBody2D) -> void:
 
 func _process(delta: float) -> void:
 	if entered and my_scene:
-		get_tree().change_scene_to_packed(my_scene)
+		get_tree().change_scene_to_file(my_scene)
