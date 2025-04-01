b3579bb2f1af46d9a274f4500502d16e9c47e6d5
Goghocutter <veerapaj9@gmail.com>
2025-04-01 22:00:14 +0700

    Revert "Merge branch 'Codes-updates' of https://github.com/LegosiHM/DigitalDesignGameProject into Codes-updates"
    This reverts commit 7055196dea3205a1f717e65c6a721187110d4a41, reversing
    changes made to c0359e5b6a162a9c4d8f2f9f07af9a50b704e938.

--- scripts/Managers/respawn_manager.gd
+++ scripts/Managers/respawn_manager.gd
@@ -8,20 +8,7 @@ func set_respawn_position(position: Vector2, scene: PackedScene) -> void:
 	respawn_scene = scene
 
 func respawn_player() -> void:
-	var scene = get_tree().current_scene
-	var blur_effect = scene.get_node("RespawnEffect")
-	var material := blur_effect.material as ShaderMaterial
-
-	blur_effect.visible = true
-
-	var tween := get_tree().create_tween()
-	tween.tween_property(material, "shader_param/blur_strength", 10.0, 0.3)
-	tween.tween_property(material, "shader_param/blur_strength", 0.0, 0.3)
-
-	await tween.finished
-
-	blur_effect.visible = false
 	if respawn_scene:
 		get_tree().change_scene_to(respawn_scene)
-		await get_tree().process_frame
+
 		get_tree().current_scene.get_node("Player").global_position = respawn_position
